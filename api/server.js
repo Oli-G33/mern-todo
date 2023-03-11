db1b1b71e540072126c06d2c4cab5846dd9c608d
OliGarc333 <ngarcia333@gmail.com>
2023-03-11 12:56:38 +0100

    Changed CORS settings

--- api/server.js
+++ api/server.js
@@ -7,7 +7,12 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.CLIENT_URL
+  })
+);
 
 const connect = async () => {
   try {
